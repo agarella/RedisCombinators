1986d4f67b38eeacf46d328b7503ae0e86a44d60
Alex Garella <agarella@gmail.com>
2015-09-25 21:25:16 +0200

    Add new combinators

--- src/main/scala/RedisOps.scala
+++ src/main/scala/RedisOps.scala
@@ -4,16 +4,26 @@ import scalaz.Scalaz._
 
 object RedisOps {
 
-  implicit class RedisOps(c: RedisClient) {
+  implicit class Redis(redis: RedisClient) {
 
-    def forEachKey[B](f: String => B): List[B] = forEachKey(f, "*")
+    def set[A](kvs: List[(String, A)]) = if (kvs.nonEmpty) redis.mset(kvs: _*)
 
-    def forEachKey(pattern: String): List[String] = forEachKey(identity, pattern)
+    def del(ks: List[String]) = if (ks.nonEmpty) redis.del(ks.head, ks.tail: _*)
 
-    def forEachKey[B](f: String => B, pattern: String): List[B] = {
-      var (cursor, res) = scan(0, f, pattern, c)
+    def getKeys: List[String] = getKeys("*")
+
+    def getKeys(pattern: String): List[String] = mapKey("*")(identity)
+
+    def forEachKey(f: String => Unit): Unit = mapKey(f)
+
+    def forEachKey(pattern: String)(f: String => Unit): Unit = mapKey(pattern)(f)
+
+    def mapKey[B](f: String => B): List[B] = mapKey("*")(f)
+
+    def mapKey[B](pattern: String)(f: String => B): List[B] = {
+      var (cursor, res) = scan(0, f, pattern, redis)
       while (cursor > 0) {
-        val (newCursor, newRes) = scan(cursor, f, pattern, c)
+        val (newCursor, newRes) = scan(cursor, f, pattern, redis)
         cursor = newCursor
         res = res |+| newRes
       }
@@ -24,7 +34,7 @@ object RedisOps {
       client.scan(cursor, pattern).map { t =>
         val newCursor = t._1.orZero
         val bs = t._2.map { xs => xs.flatten.map { key => f(key) } }.orZero
-        (newCursor, bs)
+        newCursor -> bs
       }.orZero
   }
 }
