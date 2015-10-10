65d8998b4823fe08172ff7c1d66ec428b2a335c9
Alex Garella <agarella@gmail.com>
2015-10-10 10:52:07 +0200

    Refactor
    - API rename
    - Make mapKeyStream async
    - Fix mGetStream to use pattern

--- src/main/scala/com/rediscombinators/RedisRxOps.scala
+++ src/main/scala/com/rediscombinators/RedisRxOps.scala
@@ -2,16 +2,17 @@ package com.rediscombinators
 
 import com.redis.RedisClient
 import com.redis.serialization.{Format, Parse}
+import com.rediscombinators.RedisAsyncOps._
 import rx.lang.scala.{Observable, Subscriber}
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scalaz.Scalaz._
 
 object RedisRxOps {
 
   implicit class RedisRx(rc: RedisClient) {
 
-    import RedisAsyncOps._
 
     def getStream[A](key: String)(implicit format: Format, parse: Parse[A]): Observable[A] = Observable.from(rc.getAsync(key))
 
@@ -19,25 +20,27 @@ object RedisRxOps {
     def mGetStream[A](implicit format: Format, parse: Parse[A]): Observable[A] = mGetStream("*")
 
     // TODO test with Int
-    def mGetStream[A](pattern: String)(implicit f: Format, p: Parse[A]): Observable[A] = getKeysRx.flatMap(k => getStream(k)(f, p))
+    def mGetStream[A](pattern: String)(implicit f: Format, p: Parse[A]): Observable[A] = getKeyStream(pattern).flatMap(k => getStream(k)(f, p))
 
-    def getKeysRx: Observable[String] = getKeysRx("*")
+    def getKeyStream: Observable[String] = getKeyStream("*")
 
-    def getKeysRx(pattern: String): Observable[String] = mapKeyRx(pattern)(identity)
+    def getKeyStream(pattern: String): Observable[String] = mapKeyStream(pattern)(identity)
 
-    def mapKeyRx[B](f: String => B): Observable[B] = mapKeyRx("*")(f)
+    def mapKeyStream[B](f: String => B): Observable[B] = mapKeyStream("*")(f)
 
-    def mapKeyRx[B](pattern: String)(f: String => B): Observable[B] = Observable { subscriber =>
-      implicit val c: RedisClient = rc
-      implicit val p: String = pattern
-      implicit val s: Subscriber[B] = subscriber
+    def mapKeyStream[B](pattern: String)(f: String => B): Observable[B] = Observable { subscriber =>
+      Future {
+        implicit val c: RedisClient = rc
+        implicit val p: String = pattern
+        implicit val s: Subscriber[B] = subscriber
 
-      var cursor = 0
-      do {
-        cursor = scan(cursor, f)
-      } while (cursor > 0)
+        var cursor = 0
+        do {
+          cursor = scan(cursor, f)
+        } while (cursor > 0)
 
-      subscriber.onCompleted()
+        subscriber.onCompleted()
+      }
     }
 
     private def scan[B](cursor: Int, f: String => B)(implicit pattern: String, subscriber: Subscriber[B]): Int =
