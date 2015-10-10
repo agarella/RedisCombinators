30aaa7f6358d7c4ffbd9817c90dcfbac6c9d33ec
Alex Garella <agarella@gmail.com>
2015-10-10 14:26:08 +0200

    Change env variable REDIS_SERVER_HOSTNAME to REDIS_SERVER_HOST

--- src/test/scala/com/rediscombinators/Environment.scala
+++ src/test/scala/com/rediscombinators/Environment.scala
@@ -2,7 +2,7 @@ package com.rediscombinators
 
 object Environment {
 
-  lazy val host = scala.util.Properties.envOrElse("REDIS_SERVER_HOSTNAME", "localhost" )
+  lazy val host = scala.util.Properties.envOrElse("REDIS_SERVER_HOST", "localhost" )
 
   lazy val port = Integer.parseInt(scala.util.Properties.envOrElse("REDIS_SERVER_PORT", "6379"))
 
